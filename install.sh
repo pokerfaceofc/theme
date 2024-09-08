50dd47a8320d31b5de6acec91bf4a3b846b2a3a6
ISNA GANTENG <156848233+pokerfaceofc@users.noreply.github.com>
2024-09-08 13:23:03 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -52,25 +52,6 @@ install_jq() {
   sleep 1
   clear
 }
-#Check user token
-check_token() {
-  echo -e "                                                       "
-  echo -e "${BLUE}[+] =============================================== [+]${NC}"
-  echo -e "${BLUE}[+]               LICENSY FADHOST OFFC             [+]${NC}"
-  echo -e "${BLUE}[+] =============================================== [+]${NC}"
-  echo -e "                                                       "
-  echo -e "${YELLOW}MASUKAN AKSES TOKEN :${NC}"
-  read -r USER_TOKEN
-
-  if [ "$USER_TOKEN" = "124850" ]; then
-    echo -e "${GREEN}AKSES BERHASIL${NC}}"
-  else
-    echo -e "${GREEN}Buy dulu Gih Ke Fadhost${NC}"
-    exit 1
-  fi
-  clear
-}
-
 # Install theme
 install_theme() {
   while true; do
